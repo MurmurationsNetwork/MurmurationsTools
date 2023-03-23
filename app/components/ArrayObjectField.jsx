5ec641a7592e947083bb715c738d8da59eec03a8
TingSyuanWang <vincent@fishboneapps.com>
2023-03-23 14:52:34 -0400

    feat: render multiply layer of array and object
    Signed-off-by: TingSyuanWang <vincent@fishboneapps.com>

--- app/components/ArrayObjectField.jsx
+++ app/components/ArrayObjectField.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import ArrayField from '../components/ArrayField'
 
 export default function ArrayObjectField({
   fieldName,
@@ -37,30 +38,60 @@ export default function ArrayObjectField({
       {inputValues?.map((props, index) => {
         return (
           <div key={fieldName + '.' + index}>
-            {Object?.keys(props)?.map(prop => (
-              <div key={prop + '.' + index}>
-                <legend className="block text-md font-bold mt-2">
-                  {properties[prop]?.title}
-                  {requiredProperties?.includes(prop) ? (
-                    <span className="text-red-500 dark:text-red-400">*</span>
-                  ) : (
-                    <></>
-                  )}
-                </legend>
-                <div className="flex justify-around items-center">
-                  <input
-                    type="text"
-                    value={props[prop]}
-                    name={fieldName + '[' + index + '].' + prop}
-                    aria-label={fieldName + '[' + index + '].' + prop}
-                    id={fieldName + '[' + index + '].' + prop}
-                    onChange={event => handleChange(event, index, prop)}
-                    className="form-input w-full dark:bg-slate-700 mr-2"
+            {Object?.keys(props)?.map(prop =>
+              properties[prop]?.type === 'array' &&
+              properties[prop]?.items?.type === 'object' ? (
+                <fieldset
+                  className="border-dotted border-4 border-slate-300 p-4 my-4"
+                  key={prop + '.' + index}
+                >
+                  <ArrayObjectField
+                    fieldName={fieldName + '[' + index + '].' + prop}
+                    properties={properties[prop]?.items?.properties}
+                    requiredProperties={properties[prop]?.items?.required}
                   />
+                </fieldset>
+              ) : properties[prop]?.type === 'array' ? (
+                <fieldset
+                  className="border-dotted border-4 border-slate-300 p-4 my-4"
+                  key={prop + '.' + index}
+                >
+                  <legend className="block text-md font-bold mt-2">
+                    {properties[prop]?.title}
+                  </legend>
+                  <div className="text-xs">{properties[prop]?.description}</div>
+                  <ArrayField
+                    fieldName={fieldName + '[' + index + '].' + prop}
+                    fieldType="text"
+                  />
+                </fieldset>
+              ) : (
+                <div key={prop + '.' + index}>
+                  <legend className="block text-md font-bold mt-2">
+                    {properties[prop]?.title}
+                    {requiredProperties?.includes(prop) ? (
+                      <span className="text-red-500 dark:text-red-400">*</span>
+                    ) : (
+                      <></>
+                    )}
+                  </legend>
+                  <div className="flex justify-around items-center">
+                    <input
+                      type="text"
+                      value={props[prop]}
+                      name={fieldName + '[' + index + '].' + prop}
+                      aria-label={fieldName + '[' + index + '].' + prop}
+                      id={fieldName + '[' + index + '].' + prop}
+                      onChange={event => handleChange(event, index, prop)}
+                      className="form-input w-full dark:bg-slate-700 mr-2"
+                    />
+                  </div>
+                  <span className="text-xs">
+                    {properties[prop]?.description}
+                  </span>
                 </div>
-                <span className="text-xs">{properties[prop]?.description}</span>
-              </div>
-            ))}
+              )
+            )}
             <button
               onClick={() => handleRemove(index)}
               className="rounded-full bg-yellow-500 dark:bg-green-200 hover:bg-yellow-400 dark:hover:bg-green-100 text-white dark:text-gray-800 font-bold py-2 px-4 my-4"
