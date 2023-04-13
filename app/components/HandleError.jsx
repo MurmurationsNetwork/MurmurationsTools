56348306596f8c835638537a8f9130705946badc
Geoff Turk <geoff@ic3.dev>
2023-04-13 10:38:19 +0200

    refactor: route errors already handled in all routes

--- app/components/HandleError.jsx
+++ app/components/HandleError.jsx
@@ -2,6 +2,7 @@ import { isRouteErrorResponse } from '@remix-run/react'
 
 export default function HandleError(error) {
   console.log(error)
+
   if (isRouteErrorResponse(error)) {
     return (
       <div className="container mx-auto px-4 h-screen flex items-center flex-col">
@@ -21,11 +22,11 @@ export default function HandleError(error) {
     return (
       <div className="container mx-auto px-4 h-screen flex items-center flex-col">
         <span className="text-5xl md:text-8xl mt-8 md:mt-16">😱</span>
-        <h1 className="text-md md:text-3xl font-bold mt-8 md:mt-16">
+        <h1 className="text-md md:text-3xl font-bold mt-4 md:mt-8">
           A fatal error has occurred and was logged
         </h1>
         <code className="text-sm md:text-lg mt-4 md:mt-8">
-          {error instanceof Error ? error.message : error}
+          {error instanceof Error ? error.message : JSON.stringify(error)}
         </code>
       </div>
     )
