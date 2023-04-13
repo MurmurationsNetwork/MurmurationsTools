4df5538315944d04ca3f30e0e3241cf28c4d1b4f
Geoff Turk <geoff@ic3.dev>
2023-04-13 10:13:57 +0200

    fix: uncomment config

--- remix.config.js
+++ remix.config.js
@@ -5,8 +5,8 @@ module.exports = {
   // so we default back to the standard build output.
   server: process.env.NODE_ENV === 'development' ? undefined : './server.js',
   ignoredRouteFiles: ['**/.*'],
-  // appDirectory: "app",
-  // assetsBuildDirectory: "public/build",
+  appDirectory: 'app',
+  assetsBuildDirectory: 'public/build',
   publicPath: '/build/',
   serverBuildPath: 'api/index.js',
   serverMainFields: ['main', 'module'],
