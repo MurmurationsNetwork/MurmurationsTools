07a84a61bef8cf471ad0af7ecc27370d8eccb513
TingSyuanWang <vincent@fishboneapps.com>
2023-05-30 00:54:07 -0400

    fix: fix the multiple schemas is object type and it will break the code
    Signed-off-by: TingSyuanWang <vincent@fishboneapps.com>

--- app/utils/parseRef.server.js
+++ app/utils/parseRef.server.js
@@ -3,6 +3,9 @@ import { parseSchemas } from '@murmurations/jsig'
 export default async function parseRefServer(schemaName) {
   const url = `${process.env.PUBLIC_LIBRARY_URL}/v2/schemas`
   try {
+    if (typeof schemaName === 'object') {
+      schemaName = schemaName.join(',')
+    }
     return await parseSchemas(url, schemaName)
   } catch (err) {
     throw new Response(`Schema Parse error: ${err}`, {
