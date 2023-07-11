ec5df62444641409870a09bc1af0053c3c8c6a12
Geoff Turk <geoff@ic3.dev>
2023-07-11 10:08:44 +0200

    fix: make files consistent

--- remix.config.js
+++ remix.config.js
@@ -1,8 +1,5 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
-  // When running locally in development mode, we use the built in remix
-  // server. This does not understand the vercel lambda module format,
-  // so we default back to the standard build output.
   ignoredRouteFiles: ['**/.*'],
   appDirectory: 'app',
   assetsBuildDirectory: 'public/build',
