6e158821256c0075c4ecdfee830fb9c63b5d6991
Vincent Wang <vincent@fishboneapps.com>
2023-07-11 04:11:04 -0400

    Make the setting consistent with Map (#28)

--- remix.config.js
+++ remix.config.js
@@ -1,14 +1,9 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
-  // When running locally in development mode, we use the built in remix
-  // server. This does not understand the vercel lambda module format,
-  // so we default back to the standard build output.
-  server: process.env.NODE_ENV === 'development' ? undefined : './server.js',
   ignoredRouteFiles: ['**/.*'],
   appDirectory: 'app',
   assetsBuildDirectory: 'public/build',
   publicPath: '/build/',
-  serverBuildPath: 'api/index.js',
   serverMainFields: ['main', 'module'],
   serverModuleFormat: 'cjs',
   serverPlatform: 'node',
@@ -17,6 +12,7 @@ module.exports = {
     v2_errorBoundary: true,
     v2_routeConvention: true,
     v2_meta: true,
-    v2_normalizeFormMethod: true
+    v2_normalizeFormMethod: true,
+    v2_headers: true
   }
 }
