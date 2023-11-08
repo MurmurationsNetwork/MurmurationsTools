f0574eafbc544014a6b00d75a37a887234fef976
TingSyuanWang <vincent@fishboneapps.com>
2023-11-07 22:56:32 -0500

    refactor: update error component
    Signed-off-by: TingSyuanWang <vincent@fishboneapps.com>

--- app/components/HandleError.jsx
+++ app/components/HandleError.jsx
@@ -18,17 +18,19 @@ export default function HandleError(error) {
         <code className="mt-4 text-sm md:mt-8 md:text-lg">{error.data}</code>
       </div>
     )
-  } else {
+  } else if (error instanceof Error) {
     return (
       <div className="container mx-auto flex h-screen flex-col items-center px-4">
         <span className="mt-8 text-5xl md:mt-16 md:text-8xl">😱</span>
         <h1 className="text-md mt-4 font-bold md:mt-8 md:text-3xl">
           A fatal error has occurred and was logged
         </h1>
-        <code className="mt-4 text-sm md:mt-8 md:text-lg">
-          {error instanceof Error ? error.message : JSON.stringify(error)}
-        </code>
+        <p>{error.message}</p>
+        <p>The stack trace is:</p>
+        <code className="mt-4 text-sm md:mt-8 md:text-lg">{error.stack}</code>
       </div>
     )
+  } else {
+    return <h1>Unknown Error</h1>
   }
 }
