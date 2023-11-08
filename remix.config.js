d50651272957eb829a7cf99c7543e2d2a00f6e93
TingSyuanWang <vincent@fishboneapps.com>
2023-11-07 23:06:28 -0500

    refactor: remove default values from remix.config.js
    Signed-off-by: TingSyuanWang <vincent@fishboneapps.com>

--- remix.config.js
+++ remix.config.js
@@ -1,11 +1,5 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   ignoredRouteFiles: ['**/.*'],
-  appDirectory: 'app',
-  assetsBuildDirectory: 'public/build',
-  publicPath: '/build/',
-  serverMainFields: ['main', 'module'],
-  serverModuleFormat: 'cjs',
-  serverPlatform: 'node',
-  serverMinify: false
+  serverModuleFormat: 'cjs'
 }
