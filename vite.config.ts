86cd67ad9cd2e7e63c82bf18596cd41063b8b0a4
vthwang <vincent@fishboneapps.com>
2024-07-21 12:39:09 -0700

    fix: fix console.log error
    Signed-off-by: vthwang <vincent@fishboneapps.com>

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  server: {
+    port: 3000,
+  },
   ssr: {
     noExternal: [
       "@murmurations/jsig",
